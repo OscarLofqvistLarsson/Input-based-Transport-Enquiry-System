ea41aabd6aaab9639564499390e3624be2fd913b
samuelek01 <ek.samuel@icloud.com>
2024-05-04 14:14:29 +0200

    Create tables for db

--- code/tables.py
+++ code/tables.py
@@ -1,5 +1,6 @@
 def create_tables():
 
+    # Table for a specific ticket with a ID as key
     CREATE_TICKET_TABLE = """
         CREATE TABLE IF NOT EXIST ticket(
             ticketID INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,23 +9,45 @@ def create_tables():
             numberOf INT NOT NULL
         )
     """
+
+    # Table for peoples
     CREATE_PEOPLE_TABLE = """
         CREATE TABLE IF NOT EXIST people(
-            threshold INT NOT NULL,
+            threshold INT NOT NULL, 
             speed INT NOT NULL,
             location VARCHAR(255) NOT NULL,
             destination VARCHAR(255) NOT NULL,
             funds INT NOT NULL,
-            ticketID INT,
-            FOREIGN KEY (ticketID) REFERENCES ticket(ticketID)
+            people_ticketID INT,
+            FOREIGN KEY (people_ticketID) REFERENCES ticket(ticketID)
             UNIQUE KEY (ticketID)
         )
     """
 
+    # One specific trainstaion
     CREATE_TRAIN_TABLE = """
         CREATE TABLE IF NOT EXIST train(
             ID INT AUTO_INCREMENT PRIMARY KEY,
-            schedule #hur skulle denna fungera?
+            location VARCHAR(255) NOT NULL,
+            available BOOL NOT NULL,
         )
-
+        """
+    
+    # One specific bus-station
+    CREATE_BUS_TABLE = """
+        CREATE TABLE IF NOT EXIST bus(
+            ID INT AUTO_INCREMENT PRIMARY KEY,
+            location VARCHAR(255) NOT NULL,
+            available BOOL NOT NULL,
+        )
+        """
+    
+    # Time table  
+    CREATE_SCHEDULE_TABLE = """
+        CREATE TABLE IF NOT EXIST schedule(
+            depature_time DATETIME NOT NULL,
+            arrival_time DATETIME NOT NULL, 
+            total VARCHAR(255) NOT NULL, 
+            end_station VARCHAR(255) NOT NULL,
+        )
         """
