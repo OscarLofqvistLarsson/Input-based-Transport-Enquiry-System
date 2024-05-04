b3e4bf62ac763190f5f7d6113363e05bfb4ebb97
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2024-05-04 11:40:22 +0200

    update table

--- code/tables.py
+++ code/tables.py
@@ -7,4 +7,18 @@ def create_tables():
             price INT NOT NULL,
             numberOf INT NOT NULL
         )
-"""
+    """
+    CREATE_PEOPLE_TABLE = """
+        CREATE TABLE IF NOT EXIST people(
+            threshold INT NOT NULL,
+            speed INT NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            destination VARCHAR(255) NOT NULL,
+            funds INT NOT NULL,
+            ticketID INT,
+            FOREIGN KEY (ticketID) REFERENCES ticket(ticketID)
+            UNIQUE KEY (ticketID)
+        )
+    """
+
+    CREATE_TRAIN_TABLE
