cc535860b9f734ad33d7dfeb9e4925bd933c7dd7
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2024-05-04 10:52:42 +0200

    first table

--- code/tables.py
+++ code/tables.py
@@ -1,10 +1,10 @@
 def create_tables():
 
     CREATE_TICKET_TABLE = """
-
-
-
-
-
-
+        CREATE TABLE IF NOT EXIST ticket(
+            ticketID INT AUTO_INCREMENT PRIMARY KEY,
+            destination VARCHAR(255) NOT NULL,
+            price INT NOT NULL,
+            numberOf INT NOT NULL
+        )
 """
