f899d428778974557f594bba8f2279590abe8ff1
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2024-05-05 12:07:59 +0200

    drop all

---
+++ code/dropall.py
@@ -0,0 +1,24 @@
+from connection import *
+
+
+db_connection = establish_db_connection()
+
+if db_connection:
+
+    db_cursor = db_connection.cursor()
+
+    db_cursor.execute("SHOW TABLES")
+
+    tables = db_cursor.fetchall()
+
+    for table in tables:
+        table_name = table[0]
+        drop_query = f"DROP TABLE IF EXISTS {table_name}"
+        db_cursor.execute(drop_query)
+        print(f"Dropped table: {table_name}")
+
+    db_connection.commit()
+
+    close_db_connection(db_connection)
+
+print("All tables have been dropped")
